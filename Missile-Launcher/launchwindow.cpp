a6de476f95f309e1979026fde515aadf4f4df512
Travis Lane <travis.lane.osx@gmail.com>
2014-05-17 17:33:10 -0700

    Working on cleaning up code a bit more

--- Missile-Launcher/launchwindow.cpp
+++ Missile-Launcher/launchwindow.cpp
@@ -5,6 +5,7 @@ LaunchWindow::LaunchWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::LaunchWindow) {
   ui->setupUi(this);
+  disableLauncherButtons();
 }
 
 LaunchWindow::~LaunchWindow() {
@@ -12,6 +13,99 @@ LaunchWindow::~LaunchWindow() {
 }
 
 void LaunchWindow::on_scanButton_clicked() {
+  ui->launcherListWidget->clear();
+  if(launcher_array != NULL) {
+    ml_free_launcher_array(launcher_array);
+    launcher_array = NULL;
+  }
   ml_get_launcher_array(&launcher_array, &launcher_count);
+  for(uint32_t i = 0; i < launcher_count; i++) {
+    QListWidgetItem *newItem = new QListWidgetItem(QString::number(i, 10));
+    newItem->setData(this->listWidgetRole, i);
+    ui->launcherListWidget->addItem(newItem);
+  }
   //TODO check for bad status
+}
+
+void LaunchWindow::disableLauncherButtons() {
+  ui->upButton->setEnabled(false);
+  ui->downButton->setEnabled(false);
+  ui->rightButton->setEnabled(false);
+  ui->leftButton->setEnabled(false);
+  ui->fireButton->setEnabled(false);
+}
+
+void LaunchWindow::enableLauncherButtons() {
+  ui->upButton->setEnabled(true);
+  ui->downButton->setEnabled(true);
+  ui->rightButton->setEnabled(true);
+  ui->leftButton->setEnabled(true);
+  ui->fireButton->setEnabled(true);
+}
+
+void LaunchWindow::on_launcherListWidget_itemSelectionChanged() {
+  stopAll();
+  if(ui->launcherListWidget->selectedItems().count() == 0) {
+    disableLauncherButtons();
+  } else {
+    enableLauncherButtons();
+  }
+}
+
+void LaunchWindow::on_fireButton_clicked() {
+  fireOne();
+}
+
+void LaunchWindow::on_rightButton_clicked() {
+  moveRight();
+}
+
+void LaunchWindow::on_leftButton_clicked() {
+  moveLeft();
+}
+
+void LaunchWindow::on_downButton_clicked() {
+  moveDown();
+}
+
+void LaunchWindow::on_upButton_clicked() {
+  moveUp();
+}
+
+void LaunchWindow::fireOne() {
+  stopAll();
+  foreach(QListWidgetItem *item, ui->launcherListWidget->selectedItems()) {
+    bool ok = false;
+    uint launcher_index = 0;
+    ml_launcher_t *cur_launcher = NULL;
+    launcher_index = item->data(listWidgetRole).toUInt(&ok);
+    if(ok) {
+      cur_launcher = this->launcher_array[launcher_index];
+      ml_fire_launcher(cur_launcher);
+    }
+  }
+}
+
+void LaunchWindow::stopAll() {
+
+}
+
+void LaunchWindow::moveLeft() {
+  stopAll();
+
+}
+
+void LaunchWindow::moveRight() {
+  stopAll();
+
+}
+
+void LaunchWindow::moveUp() {
+  stopAll();
+
+}
+
+void LaunchWindow::moveDown() {
+  stopAll();
+
 }
--- Missile-Launcher/launchwindow.h
+++ Missile-Launcher/launchwindow.h
@@ -18,11 +18,29 @@ public:
 
 private slots:
   void on_scanButton_clicked();
+  void on_launcherListWidget_itemSelectionChanged();
+
+  void on_fireButton_clicked();
+  void on_rightButton_clicked();
+  void on_upButton_clicked();
+  void on_leftButton_clicked();
+  void on_downButton_clicked();
 
 private:
   Ui::LaunchWindow *ui;
-  ml_launcher_t **launcher_array;
+  ml_launcher_t **launcher_array = NULL;
   uint32_t launcher_count;
+  static const int listWidgetRole = Qt::UserRole + 1;
+
+  void enableLauncherButtons();
+  void disableLauncherButtons();
+
+  void moveLeft();
+  void moveRight();
+  void moveUp();
+  void moveDown();
+  void fireOne();
+  void stopAll();
 };
 
 #endif // LAUNCHWINDOW_H
